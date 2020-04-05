8e404e633f0ff8483c3a12e821d2574573e82247
Chris Goodwin <cjg199@hotmail.co.uk>
2020-04-05 16:38:21 +0100

    Changed adc_thread_entry.c
    Changed adc_thread_entry.c into the code template.

--- Debug/src/adc_thread_entry.d
+++ Debug/src/adc_thread_entry.d
@@ -1,5 +1,5 @@
 src/adc_thread_entry.o: ../src/adc_thread_entry.c \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/adc_thread.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\api/sf_adc_periodic_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\bsp/bsp_api.h \
  c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\inc\ssp_common_api.h \
  c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\inc\ssp_version.h \
@@ -56,11 +56,18 @@ src/adc_thread_entry.o: ../src/adc_thread_entry.c \
  c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\src\bsp\mcu\all\bsp_feature.h \
  c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api.h \
  c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_adc_api.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_timer_api.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\el/tx_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\el/tx_port.h \
  c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\inc\framework\el\cm4_gcc\tx_port.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\framework\el/tx_user.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\el/tx_cmsis.h \
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/adc_thread.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/hal_data.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/common_data.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_cgc.h \
@@ -68,23 +75,16 @@ src/adc_thread_entry.o: ../src/adc_thread_entry.c \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_cgc_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_elc.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_elc_cfg.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_ioport.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_ioport_cfg.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_dtc.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_dtc_cfg.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_gpt.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_timer_api.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_adc.h \
  C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_adc_cfg.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_adc_api.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\instances/sf_adc_periodic.h \
- C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\api/sf_adc_periodic_api.h
+ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\instances/sf_adc_periodic.h
 
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/adc_thread.h:
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\api/sf_adc_periodic_api.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\bsp/bsp_api.h:
 
@@ -198,6 +198,18 @@ c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\src\bsp\mcu\all\bsp_mcu_api
 
 c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\src\bsp\mcu\all\bsp_error_checking.h:
 
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_adc_api.h:
+
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h:
+
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h:
+
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_timer_api.h:
+
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h:
+
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h:
+
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\el/tx_api.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\el/tx_port.h:
@@ -207,6 +219,8 @@ c:\users\cjg19\documents\github\adc_s5d9\synergy\ssp\inc\framework\el\cm4_gcc\tx
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\framework\el/tx_user.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\el/tx_cmsis.h:
+
+C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/adc_thread.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\src\synergy_gen/hal_data.h:
 
@@ -222,32 +236,18 @@ C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_elc.
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_elc_cfg.h:
 
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h:
-
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_ioport.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_ioport_cfg.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_dtc.h:
 
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h:
-
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_elc_api.h:
-
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_dtc_cfg.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_gpt.h:
-
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_timer_api.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\instances/r_adc.h:
 
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy_cfg\ssp_cfg\driver/r_adc_cfg.h:
 
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_adc_api.h:
-
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\driver\api/r_transfer_api.h:
-
 C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\instances/sf_adc_periodic.h:
-
-C:\Users\cjg19\Documents\GitHub\adc_s5d9\synergy\ssp\inc\framework\api/sf_adc_periodic_api.h:
--- src/adc_thread_entry.c
+++ src/adc_thread_entry.c
@@ -1,62 +1,190 @@
+/*******************************************************************************
+(C) COPYRIGHT Smart Power Solutions Limited - 2020
+
+FILE
+    adc_thread_entry
+
+ORIGINAL AUTHOR
+    Smart Power Solutions Limited
+
+DESCRIPTION
+    Enter file description
+
+REFERENCES
+    None
+
+REVIEWS
+    None
+*******************************************************************************/
+
+/*=============================================================================*
+ ANSI C & System-wide Header Files
+*=============================================================================*/
 #include <stdio.h>
-#include "adc_thread.h"
+
+/*=============================================================================*
+ Interface Header Files
+*=============================================================================*/
 #include "sf_adc_periodic_api.h"
 
-void adc_thread_entry(void);
+/*=============================================================================*
+ Local Header File
+*=============================================================================*/
+#include "adc_thread.h"
 
+/*=============================================================================*
+ Private Defines
+*=============================================================================*/
 #define EVENT_FLAG_1    0x00000001  // b 0000-0000-0000-0000-0000-0000-0000-0001
 #define EVENT_FLAG_2    0x00000002  // b 0000-0000-0000-0000-0000-0000-0000-0010
-
 #define ALL_FLAGS       (EVENT_FLAG_1 | EVENT_FLAG_2)
-uint32_t actual_flags;
-bsp_leds_t g_leds;
-uint32_t result;
-/* ADC framework example thread entry function */
+
+/*=============================================================================*
+ Private Variable Definitions (static)
+*=============================================================================*/
+static uint32_t actual_flags;
+static bsp_leds_t g_leds;
+static uint32_t result;
+
+/*=============================================================================*
+ Private Function Definitions (static)
+*=============================================================================*/
+/* None */
+
+/*=============================================================================*
+ Private Function Implementations (Static)
+*=============================================================================*/
+/* None */
+
+/*=============================================================================*
+ Public Function Implementations
+*=============================================================================*/
+/*-------------------------------------------------------------------* 
+
+  NAME
+	g_adc_framework_user_callback
+  
+  DESCRIPTION
+	Callback function for the adc thread
+	
+  PARAM
+	sf_adc_periodic_callback_args_t * p_args - Pointer to the arguments
+  
+  RETURNS
+	None
+
+*--------------------------------------------------------------------*/
+void g_adc_framework_user_callback(sf_adc_periodic_callback_args_t * p_args)
+{
+	/*
+	 * If the event is new ADC data
+	 */
+    if(p_args->event == SF_ADC_PERIODIC_EVENT_NEW_DATA)
+    {
+		/*
+		 * If the index is for event 1 then sent event 1
+		 * otherwise set flag for event two
+		 */
+        if(p_args->buffer_index == 0)
+        {
+            /*
+			 * Set the event flag
+			 */
+            tx_event_flags_set(&g_adc_flags, EVENT_FLAG_1, TX_OR);
+        }
+        else
+        {
+            /*
+			 * Set the event flag
+			 */
+            tx_event_flags_set(&g_adc_flags, EVENT_FLAG_2, TX_OR);
+        }
+    }
+}
+
+/*-------------------------------------------------------------------* 
+
+  NAME
+	adc_thread_entry
+  
+  DESCRIPTION
+	Thread entry point for the ADC Processing thread.
+	Gets the LEDs and toggles them
+	Starts the ADC scan
+	
+  PARAM
+	None
+  
+  RETURNS
+	None
+
+*--------------------------------------------------------------------*/
 void adc_thread_entry(void)
 {
+	/*
+	 * Init the Error Flag
+	 */
     ssp_err_t err = SSP_SUCCESS;
 
-    /* leds on board*/
+    /*
+	 * Connect to the LEDs on the board
+	 */
     R_BSP_LedsGet(&g_leds);
 
-    /* Initialise the ADC Framework if disable in configuration*/
+    /*
+	 * Initialise the ADC Framework if disable in configuration
+	 * Check the error flag
+	 */
     err = (g_sf_adc_periodic0.p_api->open(g_sf_adc_periodic0.p_ctrl, g_sf_adc_periodic0.p_cfg));
     if(err != SSP_SUCCESS)
     {
-        __BKPT(1);
+		__BKPT(1);
     }
 
-    /* Start the ADC scan */
-   err = (g_sf_adc_periodic0.p_api->start(g_sf_adc_periodic0.p_ctrl));
-
-   if(err != SSP_SUCCESS)
-   {
-       // Add Error correction mechanism
-       __BKPT(1);
-   }
-
-   while (1)
-   {
+    /*
+	 * Start the ADC scan
+	 */
+	err = (g_sf_adc_periodic0.p_api->start(g_sf_adc_periodic0.p_ctrl));
+	if(err != SSP_SUCCESS)
+	{
+		/*
+		 * Add Error correction mechanism
+		 */
+		__BKPT(1);
+	}
+	
+	/*
+	 * Main thread loop
+	 */
+	while (1)
+	{
            tx_event_flags_get (&g_adc_flags, ALL_FLAGS, TX_OR_CLEAR, &actual_flags, TX_WAIT_FOREVER);
 
            if( EVENT_FLAG_1 == (actual_flags & EVENT_FLAG_1))
            {
-            // process ADC data accordingly
-             for(uint32_t count=0; count<g_sf_adc_periodic0.p_cfg->sample_count; count++)
-             {
-              result = g_user_buffer[count];
-              g_ioport.p_api->pinWrite(g_leds.p_leds[0], IOPORT_LEVEL_LOW);
-              g_ioport.p_api->pinWrite(g_leds.p_leds[1], IOPORT_LEVEL_HIGH);
-             }
+				/*
+				 * process ADC data accordingly
+				 */
+				for(uint32_t count=0; count<g_sf_adc_periodic0.p_cfg->sample_count; count++)
+				{
+					result = g_user_buffer[count];
+					g_ioport.p_api->pinWrite(g_leds.p_leds[0], IOPORT_LEVEL_LOW);
+					g_ioport.p_api->pinWrite(g_leds.p_leds[1], IOPORT_LEVEL_HIGH);
+				}
            }
            else if (EVENT_FLAG_2 == (actual_flags & EVENT_FLAG_2))
            {
-            for(uint32_t count=0; count<g_sf_adc_periodic0.p_cfg->sample_count; count++)
-            {
-             result = g_user_buffer[count];
-            }
-               // process ADC data accordingly
-
+				/*
+			     * Loop storing the number of samples
+				 */
+				for(uint32_t count=0; count<g_sf_adc_periodic0.p_cfg->sample_count; count++)
+				{
+				result = g_user_buffer[count];
+				}
+				/*
+			     * process ADC data accordingly
+				 */
+				
                g_ioport.p_api->pinWrite(g_leds.p_leds[0], IOPORT_LEVEL_HIGH);
                g_ioport.p_api->pinWrite(g_leds.p_leds[1], IOPORT_LEVEL_LOW);
 
@@ -64,26 +192,9 @@ void adc_thread_entry(void)
            else
            {
            }
-      }
-}
-
-
-void g_adc_framework_user_callback(sf_adc_periodic_callback_args_t * p_args)
-{
-    if(p_args->event == SF_ADC_PERIODIC_EVENT_NEW_DATA)
-    {
-        if(p_args->buffer_index == 0)
-        {
-            /* Set the event flag */
-            tx_event_flags_set(&g_adc_flags, EVENT_FLAG_1, TX_OR);
-        }
-        else
-        {
-            /* Set the event flag */
-            tx_event_flags_set(&g_adc_flags, EVENT_FLAG_2, TX_OR);
-        }
     }
 }
 
-
-
+/*=============================================================================*
+End Of File
+*=============================================================================*/
