e222929af849dac1cb20b4618016680afaa30249
Chris Goodwin <cjg199@hotmail.co.uk>
2020-05-23 17:39:09 +0100

    tidied up thread_entry0
    Tidied up thread_entry0 and removed some unneeded code

--- src/thread0_entry.c
+++ src/thread0_entry.c
@@ -11,16 +11,11 @@ bsp_leds_t led;
 
 void thread0_entry(void)
 {
-    static bool led_on = false;
-    /* TODO: add your own code here */
     ssp_err_t status;// = SSP_SUCCESS;
 
     initialise_monitor_handles();
 
-    R_BSP_LedsGet(&led);
-    /*initialize ADC : apply power to adc,pins ,clock as per configuration settings*/
-
-    g_adc0.p_api ->open(g_adc0.p_ctrl,g_adc0.p_cfg);
+    g_adc0.p_api->open(g_adc0.p_ctrl,g_adc0.p_cfg);
 
     g_adc0.p_api->scanCfg(g_adc0.p_ctrl, g_adc0.p_channel_cfg);
 
@@ -33,10 +28,7 @@ void thread0_entry(void)
 
     while (1)
     {
-
         vTaskMODBUS();
-        g_ioport.p_api->pinWrite(led.p_leds[1], led_on);
-        led_on = !led_on;
        tx_thread_sleep (5);
     }
     g_adc0.p_api->close(g_adc0.p_ctrl);
